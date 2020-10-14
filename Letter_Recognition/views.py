77f0a123a0baf8c816b5ce5030396416501e59bc
Sujinthan <s.sujinthan@gmail.com>
2020-10-13 22:40:59 -0400

    Improve Ajax response time

--- Letter_Recognition/views.py
+++ Letter_Recognition/views.py
@@ -15,11 +15,12 @@ import numpy as np
 from keras.models import  load_model
 from keras.preprocessing.image import img_to_array
 
-
+model = load_model('model.h5')
 @app.route('/')
 @app.route('/home')
 def home():
     """Renders the home page."""
+
     return render_template(
         'index.html',
         title='Home Page',
@@ -75,7 +76,7 @@ def imgToText():
     img = prepare_image(image_data, IMG_SIZE)
 
     #load model
-    model = load_model('model.h5')
+    #model = load_model('model.h5')
     #get class names 
     y_Labels = {0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E', 5: 'F', 6: 'G', 7: 'H', 8: 'I', 9: 'J', 10: 'K', 11: 'L', 12: 'M', 13: 'N', 14: 'O', 15: 'P', 16: 'Q', 17: 'R', 18: 'S', 19: 'T', 20: 'U', 21: 'V', 22: 'W', 23: 'X', 24: 'Y', 25: 'Z', 26: 'a', 27: 'b', 28: 'd', 29: 'e', 30: 'f', 31: 'g', 32: 'h', 33: 'n', 34: 'q', 35: 'r', 36: 't'}
     #make prediction
